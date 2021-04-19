ba4c6e52ac9a90e185df143cd5eb4ba083c16fae
Driant Naun <driant.naun@bonial.com>
2021-04-19 17:12:29 +0200

    removed bc

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -98,11 +98,6 @@ function registerValidSW(swUrl: string, config?: Config) {
         };
       };
     })
-    .then((registration: any) => {
-      registration.periodicSync.register('refresh-rss-feeds', {
-        minInterval: 24 * 60 * 60 * 1000,
-      });
-    })
     .catch((error) => {
       console.error('Error during service worker registration:', error);
     });
